45e7bf382372f958e4e7b5dfa2603ab456764b42
David Bang <34190869+db2eb@users.noreply.github.com>
2019-11-06 01:21:07 -0500

    looped per item on page

--- scrape.py
+++ scrape.py
@@ -10,62 +10,64 @@ html = uClient.read()
 pagesoup = soup(html, "html.parser")
 
 # get reviews from here
-review = pagesoup.findAll("div",{"class":"hotels-community-tab-common-Card__card--ihfZB hotels-community-tab-common-Card__section--4r93H"})
+reviews = pagesoup.findAll("div",{"class":"hotels-community-tab-common-Card__card--ihfZB hotels-community-tab-common-Card__section--4r93H"})
+
+# for loop 5 times per page
+for review in reviews:
+	titleandlink = review.findAll("div",{"class":"hotels-review-list-parts-ReviewTitle__reviewTitle--2Fauz"})
+
+	# 1 show user review link. link to the review in its own page
+	reviewLink = titleandlink[0].a['href'] 
+	newlink = "https://www.tripadvisor.com"+reviewLink
+	# 2 name of hotel
+	locName = pagesoup.h1.text
+	# 3 part after the slash, from the given excel
+	parsedUrl = myurl[27:] # TODO remove 'https://www.tripadvisor.com'
+	# 4, title of review
+	reviewTitle = titleandlink[0].a.text
 
 
-titleandlink = review[0].findAll("div",{"class":"hotels-review-list-parts-ReviewTitle__reviewTitle--2Fauz"})
+	# 8, overall rating???
+	# rtemp = review[0].findAll("div",{"class":"hotels-review-list-parts-RatingLine__bubbles--1oCI4"})
+	# rating = rtemp[0].span['class'][1][7]
+	rtemp = review.findAll("div",{"class":"hotels-review-list-parts-RatingLine__bubbles--1oCI4"})[0].span['class'][1][7]
+	# bubble_10 1 stars, bubble_20 2 stars, bubble_30 3 stars, bubble_40 4 stars, bubble_50 5 stars
 
-# 1 show user review link. link to the review in its own page
-reviewLink = titleandlink[0].a['href'] 
-newlink = "https://www.tripadvisor.com"+reviewLink
-# 2 name of hotel
-locName = pagesoup.h1.text
-# 3 part after the slash, from the given excel
-parsedUrl = myurl[27:] # TODO remove 'https://www.tripadvisor.com'
-# 4, title of review
-reviewTitle = titleandlink[0].a.text
+	uClient2 = ureq(newlink)
+	html2 = uClient2.read()
+	uClient.close()
 
+	#html parsing
+	pagesoup2 = soup(html2, "html.parser")
+	review2 = pagesoup2.findAll("div",{"class":"featured-review-container"})
 
-# 8, overall rating???
-# rtemp = review[0].findAll("div",{"class":"hotels-review-list-parts-RatingLine__bubbles--1oCI4"})
-# rating = rtemp[0].span['class'][1][7]
-rtemp = review[0].findAll("div",{"class":"hotels-review-list-parts-RatingLine__bubbles--1oCI4"})[0].span['class'][1][7]
-# bubble_10 1 stars, bubble_20 2 stars, bubble_30 3 stars, bubble_40 4 stars, bubble_50 5 stars
+	# 5, username of review
+	username = review2[0].findAll("div",{"class":"info_text"})[0].div.text
+	# username = review[0].findAll("div",{"class":"social-member-event-MemberEventOnObjectBlock__event_type--3njyv"})
+	# username[0].text # this one doesnt parse
 
-uClient2 = ureq(newlink)
-html2 = uClient2.read()
-uClient.close()
+	# 6, date of review, might have to get from the reviewer link
+	date = review2[0].findAll("span",{"class":"ratingDate"})[0]['title']
 
-#html parsing
-pagesoup2 = soup(html2, "html.parser")
-review2 = pagesoup2.findAll("div",{"class":"featured-review-container"})
-
-# 5, username of review
-username = review2[0].findAll("div",{"class":"info_text"})[0].div.text
-# username = review[0].findAll("div",{"class":"social-member-event-MemberEventOnObjectBlock__event_type--3njyv"})
-# username[0].text # this one doesnt parse
-
-# 6, date of review, might have to get from the reviewer link
-date = review2[0].findAll("span",{"class":"ratingDate"})[0]['title']
-
-# 7, review itself, get from reviewer link
-fullReview = review2[0].findAll("span",{"class":"fullText"})[0].text
+	# 7, review itself, get from reviewer link
+	fullReview = review2[0].findAll("span",{"class":"fullText"})[0].text
 
 
-# 9, reviews of other things, from reviewerlink
-moreratings = review2[0].findAll("ul",{"class":"recommend"})[0]
-minireviews = moreratings.findAll("li",{"class":"recommend-answer"})
-finalans = ""
-for minireview in minireviews:
-	category = minireview.findAll("div",{"class":"recommend-description"})[0].text #name of category
-	minirating = minireview.div["class"][1][7] #rating
-	if(len(finalans)>0):
-		finalans+="$$$$"+category+"||||"+minirating
-	else:
-		finalans+=category+"||||"+minirating
-# Rooms||||5$$$$Cleanliness||||5$$$$Service||||5
+	# 9, reviews of other things, from reviewerlink
+	moreratings = review2[0].findAll("ul",{"class":"recommend"})
+	finalans=""
+	if len(moreratings)>0:
+		minireviews = moreratings[0].findAll("li",{"class":"recommend-answer"})
+		for minireview in minireviews:
+			category = minireview.findAll("div",{"class":"recommend-description"})[0].text #name of category
+			minirating = minireview.div["class"][1][7] #rating
+			if(len(finalans)>0):
+				finalans+="$$$$"+category+"||||"+minirating
+			else:
+				finalans+=category+"||||"+minirating
+		#Rooms||||5$$$$Cleanliness||||5$$$$Service||||5
 
-print(newlink,locName,parsedUrl,reviewTitle,username,date,fullReview,rtemp,finalans)
+	print(newlink,locName,parsedUrl,reviewTitle,username,date,fullReview,rtemp,finalans)
 
 
 #NOTes
